620492f665d9fed31782749c4effd93a45165c34
realvint <victor.malukhin@gmail.com>
2022-01-16 22:48:00 +0300

    add title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,17 @@ module ApplicationHelper
   def bootstrap_class_for_flash(flash_type)
     flash = FLASH_NAME[flash_type.to_sym] || flash_type.to_s
   end
+
+  def currently_at(current_page = '')
+    render partial: 'shared/header', locals: { current_page: current_page }
+  end
+
+  def full_title(page_title = '')
+    base_title = 'AskIt'
+    if page_title.present?
+      "#{page_title} | #{base_title}"
+    else
+      base_title
+    end
+  end
 end
